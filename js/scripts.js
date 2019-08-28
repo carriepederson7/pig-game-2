eed72f1939c7ce9014b4ffec0ff7fea155259e6a
Carrie Schmid and Rachael Bouisey <rachael@>
2019-08-28 15:46:43 -0700

    add ui functions to switch players

--- js/scripts.js
+++ js/scripts.js
@@ -3,33 +3,19 @@
 
 
 
-
-
-var all = new All()
-
-function All(){
-  this.eachRoll = []
-}
-
-All.prototype.addTurn = function(turn){
-  this.eachRoll.push(turn);
-}
-
-
-// function Turn(score, count, total){
-//   this.score = parseInt(score);
-//   this.count = 0
-//   this.total = []
-// }
 //
 //
-// Turn.prototype.total = function(){
-//   var total = this.count + roll();
-//   return total
-//   this.total.push(total)
+// var all = new All()
+//
+// function All(){
+//   this.eachRoll = []
+// }
+//
+// All.prototype.addTurn = function(turn){
+//   this.eachRoll.push(turn);
 // }
 
-// var score = new Score ();
+
 
 
 function Player(){
@@ -39,28 +25,39 @@ function Player(){
   // this.anotherArray = []
 }
 
-
+// function Player2(){
+//   this.currentRoll2 = 0;
+//   this.currentScore2= 0;
+//   this.totalScore= [],
+//   this.anotherArray = []
+// }
 
 Player.prototype.newRoll = function(){
   this.currentRoll= Math.floor(Math.random()*6 +1);
-
-  // return this.currentRoll
-  this.currentScore = this.currentRoll + this.currentScore;
+  if(this.currentRoll === 1){
+  this.currentScore = 0 + this.currentScore;
+  player1Turn = !player1Turn
+}else  {this.currentScore = this.currentRoll + this.currentScore;
+  }
 }
 
-
-Player.prototype.totalScore = function(){
-
-}
-
-var newPlayer = new Player()
-newPlayer.newRoll()
-// newTurn.totalScore()
-console.log(newPlayer.currentRoll)
-// console.log(newPlayer.totalScore)
-console.log(newPlayer.currentScore)
+// Player2.prototype.newRoll2 = function(){
+//   this.currentRoll2= Math.floor(Math.random()*6 +1);
+//   if(this.currentRoll2 === 1){
+//   this.currentScore2 = 0 + this.currentScore2;
+//   player1Turn = !player1Turn
+// }else  {this.currentScore2 = this.currentRoll2 + this.currentScore2;
+//   }
+// }
 
 
+// Player.prototype.totalScore = function(){
+//
+// }
+
+var player1 = new Player()
+var player2 = new Player()
+var player1Turn = true
 
 
 
@@ -69,7 +66,23 @@ console.log(newPlayer.currentScore)
 $(document).ready(function(){
   $("#click").click(function(event){
     event.preventDefault();
-    newTurn.newRoll();
+    if(player1Turn){
+    player1.newRoll();
+    $(".roll1").text(player1.currentRoll);
+    $(".total1").text(player1.currentScore);
+  }
+
+  else {
+
+    player2.newRoll();
+    $(".roll2").text(player2.currentRoll);
+    $(".total2").text(player2.currentScore);
+    }
+    if(player2.newRoll() === 1){
+      player1Turn
+    }
+
+    console.log(player1)
 
     // var roll =  function roll(){
     //   return Math.floor(Math.random()*6 +1)
